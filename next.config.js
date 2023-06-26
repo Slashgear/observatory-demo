d923c724fe261d769a3b507374a8c8d7e357a78e
Antoine <antoine395.caron@gmail.com>
2023-06-26 16:12:01 +0200

    referrer and XSS basic

--- next.config.js
+++ next.config.js
@@ -28,6 +28,14 @@ const nextConfig = {
             {
                 key: 'X-Content-Type-Options',
                 value: 'nosniff',
+            },
+            {
+                key: 'Referrer-Policy',
+                value: 'no-referrer, strict-origin-when-cross-origin'
+            },
+            {
+                key: 'X-XSS-Protection',
+                value: '1; mode=block'
             }
         ],
       },
