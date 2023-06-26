b6533ae64aa3b0dbbe73d1dcaf566b0bf8b0b11c
Antoine <antoine395.caron@gmail.com>
2023-06-26 14:47:48 +0200

    feat: better way to secure cookie

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
     const res = NextResponse.next();
-    res.cookies.set("my-cookie", "my-cookie-value", {
+    res.cookies.set("__Host-my-cookie", "my-cookie-value", {
         path: "/",
         sameSite: "strict",
         secure: true,
