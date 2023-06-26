2d8ffd913247b723e915cbea0b04ae4b433ed28e
Antoine <antoine395.caron@gmail.com>
2023-06-26 14:39:57 +0200

    feat: handle cookie correctly

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,9 @@ export async function middleware(req: NextRequest) {
     const res = NextResponse.next();
     res.cookies.set("my-cookie", "my-cookie-value", {
         path: "/",
+        sameSite: "strict",
+        secure: true,
+        httpOnly: true,
     });
     return res;
 }
